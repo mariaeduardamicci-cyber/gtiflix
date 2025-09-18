d16d8d69a6d9e14eb67f0422a04857ec19fc7d6c
camummdommgo <camundongoepico@gmail.com>
2025-09-18 10:49:18 -0300

    aaaa

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/a', function () {
+    echo 'aaaaaa';
+});
+
+Route::get ('/filmes/{filme}', function ($filme) {
+    echo 'filmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilmefilme: '. $filme;
+});
+
+Route::view('/conheca', 'sobre');
+
